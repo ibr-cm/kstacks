d0e1a915cce503938871e16449fecb46b541c2f9
johannes vb <j.van-balen@tu-bs.de>
2014-10-28 13:04:14 +0100

    reworking the video script

--- Simulator/video.sh
+++ Simulator/video.sh
@@ -1,3 +1,1 @@
-#ffmpeg -framerate 5 -pattern_type glob -i "./$1/tick*.png" -c:v libx264 -r 30 -pix_fmt yuv420p "./$1/out_5fps.mp4"
-ffmpeg -framerate 25 -pattern_type glob -i "./$1/tick*.png" -c:v libx264 -r 30 -pix_fmt yuv420p "./$1/out_25fps.mp4"
-ffmpeg -framerate 50 -pattern_type glob -i "./$1/tick*.png" -c:v libx264 -r 30 -pix_fmt yuv420p "./$1/out_50fps.mp4"
+ffmpeg -framerate $2 -pattern_type glob -i "./$1/tick*.png" -c:v libx264 -r 30 -pix_fmt yuv420p "$3/out_$2fps.mp4"
